e2025a257d986a605f981cee227914de6b5d2c82
Jan Röder <jroeder@users.fias.science>
2018-05-03 13:11:57 +0200

    compared ode solution methods

--- laem.c
+++ laem.c
@@ -44,7 +44,7 @@ int main()
        y[i1][0] = y_0[i1];
     //   printf("y[i1][0] = %5.6lf\n", y[i1][0]);    // debug statement
     }
-  for(i1 = 1; i1<=num_steps ; i1++)  // Euler algorithm (Runge Kutta 1st order)
+  for(i1 = 1; i1<=num_steps ; i1++)  // actual Runge-Kutta 4th order algorithm
     {
       double rho = r;
   
@@ -55,10 +55,31 @@ int main()
       k1[0] = tov(y[0][i1-1], y[1][i1-1], r) * tau;
       k1[1] = 4*M_PI*pow(r, 2.0) * eos(y[0][i1-1]) * tau;
 
+      // k2 = f(y(t)+(1/2)*k1 , t+(1/2)*tau) * tau
+
+      double k2[N];
+
+      k2[0] = tov(y[0][i1-1] + (1./2.)*k1[0], y[1][i1-1] + (1./2.)*k1[1], r + tau/2.)  * tau;
+      k2[1] = 4*M_PI*pow(r, 2.0) * eos(y[0][i1-1] + (1./2.)*k1[0]) * tau;
+r = rho;
+      // k3 = f(y(t)+(1/2)*k2 , t+(1/2)*tau) * tau
+
+      double k3[N];
+
+      k3[0] = tov(y[0][i1-1] + (1./2.)*k2[0], y[1][i1-1] + (1./2.)*k2[1], r + tau/2.)   * tau;
+      k3[1] = 4*M_PI*pow(r, 2.0) * eos(y[0][i1-1] + (1./2.)*k2[0]) * tau;
+r = rho;
+      // k4 = f(y(t)+k3 , t+tau) * tau
+
+      double k4[N];
+
+      k4[0] = tov(y[0][i1-1] + k3[0], y[1][i1-1] + k3[1], r + tau)  * tau;
+      k4[1] = 4*M_PI*pow(r, 2.0) * eos(y[0][i1-1] + k3[0])*tau;
+
       for(i2 = 0; i2 < N; i2++)
-	y[i2][i1] = y[i2][i1-1] + k1[i2];
+	y[i2][i1] = y[i2][i1-1] + 1./6. * (k1[i2] + 2.*k2[i2] + 2.*k3[i2] + k4[i2]);
     
-      r = rho + tau;
+r = rho + tau;
     } 
 
   for(i1 = 0; i1 <= num_steps && y[0][i1] > 0.0 ; i1++)
--- tmp.py
+++ tmp.py
@@ -3,18 +3,28 @@ import numpy as np
 import csv
 from numpy import *
 
-x = []
-y = []
+x1 = []
+y1 = []
+
+x2 = []
+y2 = []
     
 with open('tov_rk4.out', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        x.append(float(row[0]))
-        y.append(float(row[1]))
+        x1.append(float(row[0]))
+        y1.append(float(row[1]))
 
-plt.plot(x, y, label='4th order\nRunge-Kutta')
+with open('tov_euler.out', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x2.append(float(row[0]))
+        y2.append(float(row[1]))
 
-plt.title('TOV equation\nM-R-Relation')
+plt.plot(x1, y1, label='4th order\nRunge-Kutta')
+plt.plot(x2, y2, label='1st order\nEuler')
+
+plt.title('M-R-Relation\n1st vs. 4th order')
 #plt.axis([0, 0.07, 0, 11])
 plt.ylabel('M')
 plt.xlabel('R')
