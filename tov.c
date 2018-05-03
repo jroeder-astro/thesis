4922e6eeffadd661705c95d0917a7b119d063a48
Jan Röder <jroeder@users.fias.science>
2018-05-03 09:16:12 +0200

    Code gives MR Relation, but graph looks weird

--- tov.c
+++ tov.c
@@ -3,15 +3,17 @@
 #include<stdio.h>
 
 
-const int N = 2;  // Anzahl der Komponenten von \vec{y} bzw. \vec{f}.
 
-const int num_steps = 100;  // Anzahl der RK-Schritte.
+const int N = 2;            // number of components of ODE vector
+const int num_steps = 100;  // number of Runge-Kutta steps
+const double tau = 0.1;     // stepsize
+double y[N][num_steps+1];   // discretization
 
-const double tau = 0.1;  // Schrittweite.
 
-double y[N][num_steps+1];  // Diskretisierte "Bahnkurven".
 
-double y_0[N] = {0.01, 0.0};  // Anfangsbedingungen.
+double p0;                  // initial pressure
+double y_0[N] = {p0, 0.0};  // initial pressure & mass
+
 
 
 double eos(double p){
@@ -25,15 +27,24 @@ double tov(double p, double m, double r){
 }
 
 
+
 int main()
 {
+
   int i1, i2;
+
+  for(int P = 0; P < 101; P+=1  ){    // initial pressure loop
+
   double r = 0.000001;
+  double p0 = 0.01 + P * 0.001;
+  double y_0[N] = {p0, 0.0}; 
 
   for(i1 = 0; i1 < N; i1++)
-    y[i1][0] = y_0[i1];
-
-  for(i1 = 1; i1<=num_steps ; i1++)
+    {
+       y[i1][0] = y_0[i1];
+    //   printf("y[i1][0] = %5.6lf\n", y[i1][0]);    // debug statement
+    }
+  for(i1 = 1; i1<=num_steps ; i1++)  // actual Runge-Kutta 4th order algorithm
     {
       double rho = r;
   
@@ -71,11 +82,17 @@ r = rho;
 r = rho + tau;
     } 
 
-  for(i1 = 0; i1 <= num_steps; i1++)
+  for(i1 = 0; i1 <= num_steps && y[0][i1] > 0.0 ; i1++)
     {
       double radius = i1 * tau;
-      printf("%9.6lf,%9.6lf\n", radius, y[0][i1]);
+     // printf("%9.6lf,%9.6lf\n", radius, y[1][i1]);   // P(r) for one star
     }
+ // printf("**********************************************************\n");   // debug
+  
+  printf("%5.8lf,%5.8lf\n",(i1-1)*tau, y[1][i1-1]); // mass-radius for multiple stars
+
+}
+
   
   return 0;
 }
