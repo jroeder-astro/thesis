529c917d1923d01cabe52ead2b9e443fe42bfcd5
Jan Röder <jroeder@users.fias.science>
2018-05-03 12:42:52 +0200

    stuff is working well

--- tmp.py
+++ tmp.py
@@ -5,18 +5,14 @@ from numpy import *
 
 x = []
 y = []
-
-with open('mr.out', 'r') as csvfile:
+    
+with open('tov_rk4.out', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(float(row[0]))
         y.append(float(row[1]))
 
-#p1 = polyfit(x, y, 1)
-#print(p1)
-
 plt.plot(x, y, label='4th order\nRunge-Kutta')
-#plt.plot(x, polyval(p1,x))
 
 plt.title('TOV equation\nM-R-Relation')
 #plt.axis([0, 0.07, 0, 11])
