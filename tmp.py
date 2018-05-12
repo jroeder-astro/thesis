6297ee1dda08e6cac786aeeafd13de0e4281d580
badhatharry <jan@roeders-home.de>
2018-05-12 17:25:48 +0200

    Problem isn't that easy to implement

--- tmp.py
+++ tmp.py
@@ -8,31 +8,31 @@ y = []
 
 p = np.arange(0., 0.1, 0.0001)
 
-with open('inv.out', 'r') as csvfile:
+with open('tmp.out', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         x.append(float(row[0]))
         y.append(float(row[1]))
 
-plt.subplot(3,1,1)
+#plt.subplot(3,1,1)
 plt.plot(x, y, label='RK4 reconstr.')
-plt.axis([0,0.1,0,0.07])
+#plt.axis([0,0.1,0,0.07])
 plt.title('TOV equation\nReconstructed EoS')
 plt.ylabel('e(p)')
 plt.legend()
 
-plt.subplot(3,1,2)
-plt.plot(p, (p/10.)**(3./5.), label='Initial EoS')
-plt.axis([0,0.1,0,0.07])
-plt.ylabel('e(p)')
-plt.legend()
+#plt.subplot(3,1,2)
+#plt.plot(p, (p/10.)**(3./5.), label='Initial EoS')
+#plt.axis([0,0.1,0,0.07])
+#plt.ylabel('e(p)')
+#plt.legend()
 
-plt.subplot(3,1,3)
-plt.plot(p, (p/10.)**(3./5.), label='Initial EoS')
-plt.plot(x, y, label='RK4 reconstr.')
-plt.axis([0,0.1,0,0.07])
-plt.ylabel('e(p)')
-plt.legend()
+#plt.subplot(3,1,3)
+#plt.plot(p, (p/10.)**(3./5.), label='Initial EoS')
+#plt.plot(x, y, label='RK4 reconstr.')
+#plt.axis([0,0.1,0,0.07])
+#plt.ylabel('e(p)')
+#plt.legend()
 
 
 #plt.title('TOV equation\nReconstructed EoS')
