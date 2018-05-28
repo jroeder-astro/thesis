bd93d858ce1531557b86df0f6af73155f4c6f7ab
badhatharry <jan@roeders-home.de>
2018-05-28 20:51:40 +0200

    progress is ever so slow

---
+++ cpp/test.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+main(){
+
+  vector<vector<int>> vec;
+
+  vector<int> row;
+    row.push_back(5);
+    row.push_back(6);
+  vec.push_back(row);
+
+  vector<int> init;
+    init.push_back(0);
+    init.push_back(0);
+
+  for (int i = 0; i < 5; i++){
+    vec[i][];
+    
+  }
+
+  cout<<vec[0][0]<<" "<<vec[0][1]<<endl;
+  cout<<vec[1][0]<<" "<<vec[1][1]<<endl;
+
+  return 0;
+}
