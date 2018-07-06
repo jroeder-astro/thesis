75cc83c6b2c839036f1df76570a8f61a4f956396
Allg. Rel. Theo. am Comp <vartc-ss18-01@users.itp.uni-frankfurt.de>
2018-07-06 16:37:12 +0200

    fckn hell

--- why/why.cpp
+++ why/why.cpp
@@ -16,15 +16,14 @@ using namespace std;
 // Number of ODE system components
 const int N = 2;
 
-// Initial condition, y(t=0)
-// y_0[N] = {p0, m0}
-
-// double p0 = 0.000001 + 1000 * 0.0000001;
-// double y_0[N] = { p0 , 0.0 };
-
 // Equation of state
 double eos(double p){
   return pow(p/10., 3./5.);
+}
+
+// Reconstructed EoS
+double line(double p, double slope, double A){
+  return slope*p + A;
 }
 
 
@@ -32,23 +31,12 @@ double eos(double p){
 // * RK parameters *
 // *****************
 
-const int order = 4; // RK order
-const int num_steps_max = 1000000; // Max. RK steps
-const double t_max = 10.0; // radius limit (needed?)
-const double delta_abs_max = 0.001; // Max. error/step
-double tau = 0.01; // Initial coarse stepsize
-
-// double t[num_steps_max+1];     // Discrete radius "axis"
-// double y[num_steps_max+1][N];  // Discrete ODE solution
+const int num_steps_max = 1000000; // Max. RK step
+double tau = 0.01;                 // Initial stepsize
 
 // Calculates f(y(t),t) * tau.
 void f_times_tau(double *y_t, double t, 
                  double *f_times_tau_, double tau);
-
-// Do I really need this?
-void f_times_tau_rec(double *y_t, double t, 
-                     double *f_times_tau_, double tau, 
-                     double *recon);
 
 // Do Runge-Kutta
 void RK_step(double *y_t, double t, 
@@ -74,8 +62,7 @@ void one_free(int num_steps_max, double **t);
 // * Main function *
 // *****************
 
-int main(/*int argc, char **argv*/)
-{
+int main(){
 
   double d1, rho = 0.001;
   int i1, i2;
@@ -85,9 +72,9 @@ int main(/*int argc, char **argv*/)
 // Memory allocation for t "radius" axis (discrete)
 // Also for the discrete ODE solution array
 
-  vector<vector<double>> EoS;
-  vector<double> Eresult;
-  vector<double> Presult;  
+//  vector<vector<double>> EoS;
+//  vector<double> Eresult;
+//  vector<double> Presult;  
 
   vector<array<double, N>> EOS_arr;
   array<double, N> eos_tmp;
@@ -115,13 +102,10 @@ int main(/*int argc, char **argv*/)
   }
   mcount--;
 
-
 // ***************************************
 
-// Build initial EoS output vectors
-
-  double p0 = 0.00001 + mcount * 0.00001;
-  double y_0[N] = { p0 , 0.0 };
+  double p_init = 0.00001 + mcount * 0.00001;
+  double y_0[N] = {  , 0.0 };
 
   // Initialize solution.
 
@@ -132,94 +116,21 @@ int main(/*int argc, char **argv*/)
 
   // Do Runge-Kutta.
 
-  //cout << "reached RK" << endl;
-    
-
   double* y_tau;
   one_alloc(N, &y_tau);
 
-
-  for(i1 = 0;/* i1 < num_steps_max &&*/ 
-		y[i1][0] > 0.0; i1++)
+  for(i1 = 0; y[i1][0] > 0.0; i1++)
     {
       // RK steps
       
       vector<double> EOS_tmp;
       vector<double> result_tmp;      
-
-      // double* y_tau;
-      // one_alloc(N, &y_tau);
-
-      // y(t)   --> \tau   y_{\tau}(t+\tau)
-   
+  
       RK_step(y[i1], t[i1], y_tau, tau, EOS_tmp);
 
-      //cout << "about to vary stepsize" << endl;
-
-      // ******************************************
-   
-      // Check pressure difference between array points
-
-
-/*
-      if ( fabs(y_tau[0]-y[i1][0]) <= pow(10., -5.5) )
-      {
-
-          cout << "hi" << endl;
-
-	  if( y_tau[0] > y[i1][0])
-	  {
-	     tau /= 1.1;
-	     i1--;
-	  }
-
-	  if( y_tau[0] < y[i1][0])
-	  {
-	     tau *= 1.1;
-	     i1--;
-	  } 
-      }
-
-      else if ( fabs(y_tau[0]-y[i1][0]) >= pow(10., -4.5) )
-      {
-
-          cout << "yo2" <<endl;
-
-	  if( y_tau[0] > y[i1][0])
-	  {
-	     tau *= 1.1;
-	     i1--;
-	  }
-
-	  if( y_tau[0] < y[i1][0])
-	  {
-	     tau /= 1.1;
-	     i1--;
-	  } 
-      }
-
-      else
-      {
-          cout << "accept" << endl;
-
-     	  // Accepting the step
-       
-	  for(i2 = 0; i2 < N; i2++)
-	    y[i1+1][i2] = y_tau[i2];
-
-	  t[i1+1] = t[i1] + tau;
-
-          // Building the vectors          
-
-          EoS.push_back(EOS_tmp);
- 
-          eos_tmp = {y[i1][0], eos(y[i1][0])};
-          EOS_arr.push_back(eos_tmp);
-      }
-
-
-*/ // FROM HERE, ONLY UPPER BOUND...
-
+     // ******************************************
+  
+     // FROM HERE, ONLY UPPER BOUND...
 
       if( fabs(y_tau[0]-y[i1][0]) <= pow(10., -5.))	
       {
@@ -233,14 +144,10 @@ int main(/*int argc, char **argv*/)
           // Building the vectors          
 
           EoS.push_back(EOS_tmp);
-
-          // Presult.push_back(y[i1][0]);
-          // Eresult.push_back(eos(y[i1][0]));
-         
+    
           eos_tmp = {y[i1][0], eos(y[i1][0])};
           EOS_arr.push_back(eos_tmp);
 
-          // tau = rho;
 	}
 
       else
@@ -258,14 +165,7 @@ int main(/*int argc, char **argv*/)
 	     tau /= 1.1;
 	     i1--;
 	  } 
-	 // tau = tau_new;
-
-	 // i1--;
-	}
-
-      // one_free(N, &y_tau);
-      // y_tau = NULL;    
- 
+        }
     }
 
   one_free(N, &y_tau);
@@ -276,18 +176,6 @@ int main(/*int argc, char **argv*/)
   cout << endl; // WHAT IS THIS FOR??
 
   two_free(num_steps_max, N , &y);
-
-
-
-//  int num_steps = i1;
-//  cout << "hello" << endl;    
-//  cout << Presult[i1] << "  " << Eresult[i1] << endl; 
-
-//  vector<std::array<double, N>> EOS_arr;
-
-
-//  double **EoS_arr;
-//  two_alloc(Eresult.size(), N, &EoS_arr);
 
 
 /* // debug
@@ -350,37 +238,7 @@ int main(/*int argc, char **argv*/)
 // ******************************************************
 
 
-
-// allocation of new time and tov axes
-  double *t_2;
-  one_alloc(5, &t_2);
-
-  double **y_2;
-  two_alloc(8, N, &y_2);
-
-
-// debug statement
-  cout << "about to start interpolating\n";
-
-// allocating the memory for interpolated line
-  two_alloc(5, N, &REOS);
-
-// First loop: only go up to mass 2.3 (whatever the unit)
-  while (M <= 2.3)
-  { 
-    if (fscanf(TOV, "%lf,%lf", &M, &R) == EOF) break;
-cout << "end 7\n";
- 
-    // interpolation "anchor", D(e) from last 
-    Preos = EOS_arr[0][0] + 0.00001;
-    Ereos = EOS_arr[0][1];
-cout << "end 7\n";
- 
-    // Second loop: "error" checking, keeping D(p) even
-    while (fabs(y[i1][1] - M) > err)
-    { 
- cout << "end 7\n";
-    
+   
       // *********************************************    
 
       // Linear interpolation of EoS, 2D array
@@ -398,58 +256,6 @@ cout << "end 7\n";
         cout << REOS[n][0] << " " << REOS[n][1] << endl;
 
       }
-      // *********************************************
-  
-cout << "end 7\n";
- 
-      // Try reconstruction with Euler 
-      // hopefully not too crappy
-
-      double *y_tau;
-      one_alloc(N, &y_tau);
-
-      t_2[0] = 0.000000001;
-
-      // ************************************************
-      // PART ONE: Start calculating mass with the 
-      // interpolated REOS array
-
-
-      for(i1 = 0; i1 < num_steps_max &&   // !!
-                 y_2[i1][0] > 0.0; i1++)
-      {
-      //  double *y_tau;
-      //  one_alloc(N, &y_tau);
-
-        p0 = EOS_arr[0][0];
-        double y_0[N] = { p0 , 0.0 };
-
-      // Initialize solution.
-
-   cout << "end 8\n";
-
-      // t_2[0] = 0.000000001;
-
-        for(i2 = 0; i2 < N; i2++)
-            y_2[0][i2] = y_0[i2];
-
-   cout << y_2[i1] << " " << t_2[i1] << " " 
-        << y_tau << " " <<  tau << " " << REOS[i1] << endl;
-
-
-        Euler_step(y_2[i1], t_2[i1], y_tau, tau, REOS[i1]);
- 
-   cout << y_2[i1][0] << " " << y_2[i1][1] << endl;
-
-   cout << "end 13\n";
-
-
-      //  one_free(N, &y_tau);
-      }
-
-      one_free(N, &y_tau);
- 
-cout << "end 9\n";
 
       // **************************************************
 
@@ -644,82 +450,6 @@ void f_times_tau(double *y_t, double t,
   f_times_tau_[1] = tau * 4*M_PI * pow(t, 2.) *eos(y_t[0]);
 }
 
-/*
-// RK4 reconstruction function
-
-void RK_step_rec(double *y_t, double t, 
-        double *y_t_plus_tau, double tau, double *recon)
-{
-  int i1;
-
-  // Calculate k1 = f(y(t),t) * tau.
-
-  double k1[N];
-  f_times_tau_rec(y_t, t, k1, 
-                  tau, recon[0], recon[1]);
-
-  // k2 = f(y(t)+(1/2)*k1 , t+(1/2)*tau) * tau.
-
-  double y_2[N];
-
-  for(i1 = 0; i1 < N; i1++)
-    y_2[i1] = y_t[i1] + 0.5*k1[i1];
-
-  double k2[N];
-  f_times_tau_rec(y_2, t + 0.5*tau, k2, 
-                  tau, recon[2], recon[3]);
-
-  // k3
-
-  double y_3[N];
- 
-  for(i1 = 0; i1 < N; i1++)
-    y_3[i1] = y_t[i1] + 0.5*k2[i1];
-
-  double k3[N];
-  f_times_tau_rec(y_3, t + 0.5*tau, k3, 
-                  tau, recon[4], recon[5]);
-
-  // k4
-
-  double y_4[N];
- 
-  for(i1 = 0; i1 < N; i1++)
-    y_4[i1] = y_t[i1] + k3[i1];
-
-  double k4[N];
-  f_times_tau_rec(y_4, t + tau, k3, 
-                  tau, recon[6], recon[7]);
-
-  // final
-
-  for(i1 = 0; i1 < N; i1++){
-       y_t_plus_tau[i1] = y_t[i1] + 1./6. * 
-       (k1[i1] + 2.*k2[i1] + 2.*k3[i1] + k4[i1]);
-    }
-}
-*/
-
-// Calculates f*tau with EoS array(s)
-
-void f_times_tau_rec(double *y_t, double t, 
-            double *f_times_tau_, double tau, 
-            double *recon)
-{
-  if(N != 2)
-    {
-      fprintf(stderr, "Error: N != 2!\n");
-      exit(EXIT_FAILURE);
-    }
-
-  // TOV equation implementation
-  f_times_tau_[0] = tau * (  -( recon[1] + recon[0]) 
-                        * (y_t[1] + 4*M_PI*pow(t, 3.)*recon[0]) 
-                        / (-2*y_t[1]*t + pow(t, 2.0)));
-  f_times_tau_[1] = tau * 4*M_PI * pow(t, 2.) * recon[1];
-}
-
-
 // Allocating memory
 
 void one_alloc(int num_steps_max,  double **t)
@@ -821,4 +551,62 @@ void two_free(int num_steps_max, int N, double ***y)
 	}
 	}
 */
+
+// LOWER BOUND FOR RK STEP [unfinished] 
+
+/*
+      if ( fabs(y_tau[0]-y[i1][0]) <= pow(10., -5.5) )
+      {
+
+          cout << "hi" << endl;
+
+	  if( y_tau[0] > y[i1][0])
+	  {
+	     tau /= 1.1;
+	     i1--;
+	  }
+
+	  if( y_tau[0] < y[i1][0])
+	  {
+	     tau *= 1.1;
+	     i1--;
+	  } 
+      }
+
+      else if ( fabs(y_tau[0]-y[i1][0]) >= pow(10., -4.5) )
+      {
+
+          cout << "yo2" <<endl;
+
+	  if( y_tau[0] > y[i1][0])
+	  {
+	     tau *= 1.1;
+	     i1--;
+	  }
+
+	  if( y_tau[0] < y[i1][0])
+	  {
+	     tau /= 1.1;
+	     i1--;
+	  } 
+      }
+
+      else
+      {
+          cout << "accept" << endl;
+
+     	  // Accepting the step
+       
+	  for(i2 = 0; i2 < N; i2++)
+	    y[i1+1][i2] = y_tau[i2];
+
+	  t[i1+1] = t[i1] + tau;
+
+          // Building the vectors          
+
+          EoS.push_back(EOS_tmp);
+ 
+          eos_tmp = {y[i1][0], eos(y[i1][0])};
+          EOS_arr.push_back(eos_tmp);
+      }
 
