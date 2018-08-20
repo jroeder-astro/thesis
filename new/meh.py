fd23cf4d3dbe81067f69e3d039c6b1e192a121fb
Jan Röder <jroeder@users.fias.science>
2018-08-20 09:39:35 +0200

    ready for precision dialing. rk4 postponed

--- new/meh.py
+++ new/meh.py
@@ -23,9 +23,6 @@ with open('mr.out', 'r') as csvfile:
 
 plt.plot(x1, y1, label='Euler')
 
-#plt.plot([15.5, 14.99],[1.0005, 1.03217], 'ro', label='Stefan loop')
-#plt.plot([15.59],[0.994556], 'bo', label='Jan loop')
-
 with open('rec_stefan.out', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
@@ -48,44 +45,26 @@ with open('rec_stefan_rk4.out', 'r') as csvfile:
         x4.append(float(row[0]))
         y4.append(float(row[1]))
 
-plt.plot(x2, y2, 'co', label='Rec_Stefan_RK4')
+plt.plot(x4, y4, 'co', label='Rec_Stefan_RK4')
 
 
-#plt.plot([15.5, 14.99, 14.59],[1.00014, 1.03397, 1.06196], 'go', label='Rec_Jan')
-
-
-
-#with open('RK4th.dat', 'r') as csvfile:
-#    plots = csv.reader(csvfile, delimiter=' ')
-#    for row in plots:
-#        x2.append(float(row[0]))
-#        y2.append(float(row[1]))
-
-#plt.subplot(3,1,1)
-#plt.plot(x1, y1)
-#plt.plot(x2, y2, label='RK4')
-#plt.axis([0.00007,0.000076,0.0006,0.001])
-#plt.title('TOV equation')
-plt.ylabel('M')
+#plt.subplot(2,1,1)
+#plt.plot(x1, y1, label='Euler')
+#plt.plot(x2, y2, 'ro', label='Rec_Stefan')
+#plt.plot(x3, y3, 'go', label='Rec_Jan')
+#plt.ylabel('M')
 #plt.legend()
 
-#plt.subplot(3,1,2)
-#plt.plot(p, (p/10.)**(3./5.), label='Initial EoS')
-#plt.axis([0,0.1,0,0.07])
-#plt.ylabel('e(p)')
-#plt.legend()
-
-#plt.subplot(3,1,3)
-#plt.plot(p, (p/10.)**(3./5.), label='Initial EoS')
-#plt.plot(x, y, label='RK4 reconstr.')
-#plt.axis([0,0.1,0,0.07])
-#plt.ylabel('e(p)')
+#plt.subplot(2,1,2)
+#plt.plot(x1, y1, label='Euler')
+#plt.plot(x4, y4, 'ro', label='Rec_Stefan_RK4')
+#plt.plot(x3, y3, 'go', label='Rec_Jan')
+#plt.ylabel('M')
 #plt.legend()
 
 
-#plt.title('TOV equation\nReconstructed EoS')
-#plt.axis([0, 0.07, 0, 11])
-#plt.ylabel('e(p)')
+plt.title('Mass-Radius-Relation\nReconstruction Algorithms')
+plt.ylabel('M(R)')
 plt.xlabel('R')
 plt.legend()
 plt.show()
