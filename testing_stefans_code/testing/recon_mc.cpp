e4e77fdf7527f4723cb9c9b83ff74e213ee518b0
badhatharry <jan@roeders-home.de>
2018-10-20 18:08:06 +0200

    mc doesnt work

--- testing_stefans_code/testing/recon_mc.cpp
+++ testing_stefans_code/testing/recon_mc.cpp
@@ -3,7 +3,7 @@
 #include<math.h>
 #include<stdlib.h>
 #include<fstream>
-#include<omp.h>
+#include<time.h>
 using namespace std;
 
 
@@ -22,6 +22,11 @@ double eps0,epsp,epsm,slope_old=0,lambda;
 vector<double> alpha(4);
 vector<vector<double>> MR_rel;
 int    mcount  = 0;
+double p_init;
+
+
+vector<double> mc;
+
 
 // Function heads
 
@@ -42,6 +47,10 @@ int tov(double* y_0,double p_cut,vector<double> *alpha);
 // calculates the optimum star for a given slope
 void getR();
 
+void monte_carlo(int n);
+
+
+
 // Main function
 
 
@@ -61,7 +70,7 @@ main(){
 
   y=(double*)malloc(N*num_steps*sizeof(double));
 
-  double p_init  = 0.0;
+  p_init  = 0.0;
   double slope   = 1.00;
   double slope_step = -0.05;
 
@@ -71,12 +80,12 @@ main(){
   bool   flag    = false;
   double diff0   = 0.0;
   double diff    = 0.0;
-  bool first = true;
+  bool   first   = true;
   double alpha3_old;
   double e_rec;
   double pstep;
-  int indx=2;
-
+  int    indx    = 2;
+  int n = 0;
 
   // File I/O
 
@@ -86,7 +95,6 @@ main(){
   if (MRR == NULL)
     exit(0);
   while (1) {
- // mr.out also contains the corresponding pressures now
     if (fscanf(MRR, "%lf %lf %lf %*f", &R, &M, &pcenter) == EOF)
       break;
     one_MR[0] = R;
@@ -96,6 +104,24 @@ main(){
   }
   fclose(MRR);
   MRR = NULL;
+
+/*
+  // check for maxima in MRR
+
+  vector<int> maxmin;
+  double maxdiff0 = MR_rel[1][1] - MR_rel[0][1];
+  double maxdiff  = 0;
+  int    mmindx   = 0;
+  for (i1 = 2; i1 < MR_rel.size(); i1++) {
+    maxdiff = MR_rel[i1][1] - MR_rel[i1-1];
+    if (maxdiff * maxdiff0 < 0) {
+      max.push_back(i1);
+      maxdiff0 = maxdiff;
+    }
+  }
+*/
+
+  // set initial mcount
 
   for (i1 = 0; i1 < MR_rel.size(); i1++) {
     if (MR_rel[i1][1] >= 1)
@@ -108,37 +134,75 @@ main(){
 
   p_init   = MR_rel[mcount-1][2];
   p_dur    = MR_rel[mcount-1][2];
-  e_rec    = line(p_dur,p_dur, &alpha);
+  e_rec    = line(p_dur, p_dur, &alpha);
   t[0]     = 0.0000000001;
   first    = true;
-  alpha[0] = p_init;
+  alpha[0] = p_init;     // this has to be rewritten using indx
   alpha[1] = e_rec;
   indx     = alpha.size()-2;
   alpha[2] = 5 * p_init;
   alpha[3] = e_rec + (alpha[2]-alpha[0]) / slope;
   indx     = 2;
-
-// code optimizes a function eps = (R-R_wanted)**2 + lambda*(slope-slope_before)**2 do avoid big jumps in the slope
-// as the slope_before is not known in the first round, use lambda=0 for optimization
-
+  
   lambda = 0;
 
   while (mcount < 40) {
-    pstep=1e-7;
+
+    pstep = 1e-7;
     flag_s = false;
-    slope_step=-0.05;
-    double pold= alpha[alpha.size()-2];
-    double eold= alpha[alpha.size()-1];
+    slope_step = -0.05;
     double d0,dplus,dminus;
 
+    int num = 0;
 
     if (!first) {  
-      alpha.push_back(pold + 1000*pstep);
-      alpha.push_back(eold + 1000*pstep / slope);
+      alpha.push_back(alpha[alpha.size()-2] + 1000*pstep);
+      alpha.push_back(alpha[alpha.size()-1] + 1000*pstep / slope);
     }
-    indx=alpha.size()-2;
-// calculate eps for the initial slope (slope from previous mcount solution)  and a value below and above to determine whether slope_step should be positive or negative 
+    indx = alpha.size()-2;
 
+    cout << "about to enter n loop\n";
+    n = 200;
+
+    while (n <= 1600) {
+
+      monte_carlo(n);
+      cout << mc[1] << endl;
+      alpha[indx+1] = mc[1];
+      cout << alpha[indx-2] << endl;
+      getR();  
+      diff0_s = Rcomp - MR_rel[mcount][0];
+
+      printf("Rcomp : %lf; MR_rel: %f\n", Rcomp, MR_rel[mcount][0]);
+
+      for (num = 2; num < n; num += 2) {
+        alpha[indx+1] = mc[num + 1];
+        alpha[indx] = mc[num];
+        printf("a[i+1] : %lf\n", alpha[indx+1]);
+        printf("a[i] : %lf\n", alpha[indx]);
+
+        getR();
+        diff_s = Rcomp - MR_rel[mcount][0];
+
+        printf("Rcomp : %lf; MR_rel: %f\n", Rcomp, MR_rel[mcount][0]);
+
+        if (fabs(diff_s) < fabs(diff0_s)) {
+          diff0_s = diff_s;
+        }
+      } 
+
+      if (fabs(diff0_s) < 0.00001) {
+        break;
+      }
+    
+      n *= 2;
+    }
+
+    Rcomp = diff0_s + MR_rel[mcount][0];
+    printf("Rcomp : %lf; MR_rel: %f\n", Rcomp, MR_rel[mcount][0]);
+ 
+
+/*
     getR();
     diff0_s=Rcomp-MR_rel[mcount][0];
     eps0 = diff0_s*diff0_s +lambda*(slope-slope_old)*(slope-slope_old);
@@ -167,81 +231,74 @@ main(){
       else 
         slope_step = -0.05; 
     }
+*/
 
-
+/*
    first = false;
-// check wich direction to go
-
-#pragma omp parallel for ordered schedule(dynamic)   
-   for (;slope > 0.0;) {
+   while (slope > 0.0) {
       slope += slope_step;
-      if(slope<=0) 
-        break;
+
+      if(slope<=0) {
+        slope_step -= slope_step;
+        slope_step /= 10;
+        slope += slope_step;
+      }
 
       alpha3_old = alpha[indx+1];
-//      slope = (alpha[indx]-alpha[indx-2]) / (alpha[indx+1]-alpha[indx-1]); 
       alpha[indx+1] = alpha[indx-1] + (alpha[indx]-alpha[indx-2])/slope; 
 
       getR();
 
-      if(p_end>alpha[indx-2]*5.0) 
+      if(p_end>alpha[indx-2]*5.0) {
+        cout << p_end-alpha[indx-2]*5.0 << endl;
         continue;
+      }
       if(fabs(diff)>1e-4) 
         continue;
 
       diff_s = Rcomp - MR_rel[mcount][0];
       epsp = diff_s*diff_s +lambda*(slope-slope_old)*(slope-slope_old);
-      printf("diff radius %g %g %g %g %g %g %g %g \n",epsp,slope,slope_step, diff_s, 
-      Rcomp,MR_rel[mcount][0],Mcomp,MR_rel[mcount][1]);
+      printf("diff radius %g %g %g %g %g %g %g %g \n",epsp,slope,slope_step, 
+      diff_s,Rcomp,MR_rel[mcount][0],Mcomp,MR_rel[mcount][1]);
 
       if (fabs(diff_s) < 0.000001) {
-//        cout << "fabs(diff_s) break condition" << endl;
         break;
       }
-
       if (epsp < eps0) {  
-      eps0=epsp;
-//        cout << "diff0_s * diff_s > 0" << endl;
+        eps0=epsp;
         continue;
       }
-// if eps is getting bigger, decrease slope step size and flip the sign
       slope_step /= -10;
       eps0=epsp;
 
-      if (fabs(slope_step) < 1e-6) {
-        cout << "slope_step break condition" << endl;
-        break;
-      }
-//      slope-=10.*slope_step;
-
-//      alpha[indx+1] = alpha3_old;
 
     } // end slope loop
-// after first round set lambda to some reasonable value (one might play around with this number)
-    lambda=1e-2;
-    slope_old=slope;
-    alpha[indx]=y[0];
-    alpha[indx+1] = alpha[indx-1] + (alpha[indx]-alpha[indx-2]) 
-                       / slope ; 
-//  alpha[indx+1]=line(y[0],p_dur,&alpha);
+*/
+    //  lambda=1e-3;
+    //  slope_old=slope;
+    //  alpha[indx]=y[0];
+    //  alpha[indx+1] = alpha[indx-1] + (alpha[indx]-alpha[indx-2]) 
+    //                  / slope ; 
 
+    slope = (alpha[indx+1] - alpha[indx-1] ) / (alpha[indx]-alpha[indx-2]); 
 
     indx=alpha.size()-2;
 
-    fprintf(fres,"%e %e %e %e %e %e %e\n",MR_rel[mcount][0],Rcomp,MR_rel[mcount][1],Mcomp,p_end,line(p_end,p_dur,&alpha),slope);
+    fprintf(fres,"%e %e %e %e %e %e %e\n",MR_rel[mcount][0],Rcomp,
+            MR_rel[mcount][1],Mcomp,p_end,line(p_end,p_dur,&alpha),slope);
  
-
     fflush(fres);
 
     cout << "Radius: " << MR_rel[mcount][0] << " , " << Rcomp << endl;
     cout << "Mass:   " << MR_rel[mcount][1] << " , " << Mcomp << endl;
     cout << "central pressure: " << p_end << endl;
     cout << "slope: " << slope << endl;
+    cout << "mcount: " << mcount << endl;
 
     mcount++;
   } // end mcount loop
  
-  // output...
+
   free(y); 
   fclose(fres);
   return 0;
@@ -254,13 +311,10 @@ int tov(double* y_0,double p_cut,vector<double> *alpha) {
   int i1,i2;
   double y_tau[N];
 
-       // Initializing
+  // Initializing
   for (i1 = 0; i1 < N; i1++) {
     y[i1] = y_0[i1];
   }
-
-      // Integration
-      // cout << "doing Euler\n";
 
   for (i1 = 0; y[i1*N] > 0; i1++) {
     tov_euler(&y[i1*N], t[i1], y_tau, tau, p_cut, alpha);
@@ -275,15 +329,15 @@ int tov(double* y_0,double p_cut,vector<double> *alpha) {
   return i1;
 }
 
-// end of mass calculation
-
 double line(double p,double p_cut, vector<double> *alpha) {
   int i;
+
+  // comment this out for external eos input
   if(p <= p_cut) 
     return pow(p/10.,0.6);
 
   for(i=2;i<alpha->size();i+=2) {
-    if(p<(*alpha)[i]) break;
+    if(p < (*alpha)[i]) break;
   }
 
   double e =  (p - (*alpha)[i-2]) * 
@@ -320,24 +374,21 @@ void getR() {
   bool flag = false;
   double diff0,diff=0.0;
   double pstep = 1e-7;
-  p_end = alpha[indx-2]-pstep;
-  while (p_end >= 0.8 * p_dur) {
-
-    if(p_end>alpha[indx-2]*5.0) 
-      break;
-    p_end += pstep;
+  p_end = alpha[indx-2];  // hmmm not sure about this
+  //  while (p_end >= 0.8 * p_dur) {
 
     y_0[0] = p_end; 
     y_0[1] = 0.0;
 
-    i1 = tov(y_0,p_dur,&alpha);
+    i1 = tov(y_0, p_dur, &alpha);   
 
-// end of mass calculation
+    // end of mass calculation
     Rcomp = t[i1-1]+y[(i1-1)*N]*tau/(y[(i1-2)*N]-y[(i1-1)*N]);
-    Mcomp = y[(i1-1)*N+1] + 
+    Mcomp = y[(i1-1)*N+1] +  
             (y[(i1-1)*N+1]-y[(i1-2)*N+1])/tau*(Rcomp-t[i1-1]);
     Mcomp /= 1.4766;
 
+/*
     if (!flag) {
       diff0 = Mcomp - MR_rel[mcount][1];
 
@@ -345,7 +396,7 @@ void getR() {
         break;
 
       if(diff0>0) {
-         printf("masses seem to go down with increasing p_center. not yet implemented\n");
+         printf("masses seem to go down with increasing p_center.\n");
          exit(0);
       }
       flag = true;
@@ -363,7 +414,6 @@ void getR() {
       diff = diffx;
       if(fabs(diff)<1e-6) 
         break;
-//          printf("diff mass %g %g %g %g %g %g\n",pstep,slope,y[(i1-1)*N+1] / 1.4766, MR_rel[mcount][1],t[i1-1],MR_rel[mcount][0]);
 
       if (diff * diff0 > 0) {
           // cout << diff * diff0 << endl;
@@ -371,9 +421,9 @@ void getR() {
       }
 
       pstep /= 10.0;
-      diff=0.0;
-      if (pstep < 1e-10) {
-//           cout << "pstep < x breaking condition" << endl;
+      diff = 0.0;
+      if (pstep < 1e-9) {
+          // cout << "pstep < x breaking condition" << endl;
         break;
       }
  
@@ -381,11 +431,38 @@ void getR() {
       continue;
     }
 
-  } // end p_end < p_dur loop
-
+//  } // end p_end < p_dur loop
+*/
+/*
   Rcomp = t[i1-1]+y[(i1-1)*N]*tau/(y[(i1-2)*N]-y[(i1-1)*N]);
   Mcomp = y[(i1-1)*N+1] + (y[(i1-1)*N+1]-y[(i1-2)*N+1])/tau*(Rcomp-t[i1-1]);
   Mcomp /= 1.4766;
-
+*/
   return;
 }
+
+void monte_carlo(int n) {
+
+  time_t t;
+  struct tm tm;
+  srand(time(NULL)); 
+/*
+  double p_max = 0, p_min = 0, e_max = 0, e_min = 0;    
+
+  p_max = alpha[indx-2] + 1e-5;
+  p_min = alpha[indx-2] - 1e-5
+  e_max = alpha[indx-1] + 0.03;
+  e_min = alphs[indx-1] - 0.03;
+*/
+  if (n > mc.size())
+    mc.push_back(n-mc.size());
+
+  for (int mc_c = 0; mc_c <= n; mc_c += 2) {
+    mc[mc_c] = (double)rand()/(double)RAND_MAX * 2e-5 + alpha[indx-2];
+    mc[mc_c+1] = (double)rand()/(double)RAND_MAX * 0.06 + alpha[indx-1];
+  }
+  
+  return;
+}
+
+
