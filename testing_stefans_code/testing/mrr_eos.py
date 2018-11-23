fe8c7581db6c5917f78152f53d3b1fa27159d304
Jan Röder <jroeder@users.fias.science>
2018-11-23 09:42:52 +0100

    lets see if this git pushing works

--- testing_stefans_code/testing/mrr_eos.py
+++ testing_stefans_code/testing/mrr_eos.py
@@ -17,10 +17,20 @@ Ml25 = []
 el25 = []
 pl25 = []
 
+Rl255 = []
+Ml255 = []
+el255 = []
+pl255 = []
+
 Rl26 = []
 Ml26 = []
 el26 = []
 pl26 = []
+
+Rl265 = []
+Ml265 = []
+el265 = []
+pl265 = []
 
 Rl27 = []
 Ml27 = []
@@ -66,6 +76,14 @@ with open('results_l2_5', 'rb') as csvfile:
         pl25.append(float(row[4]))
         el25.append(float(row[5]))
 
+with open('results_l2_55', 'rb') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ' ')
+    for row in plots:
+        Rl255.append(float(row[1]))
+        Ml255.append(float(row[3]))
+        pl255.append(float(row[4]))
+        el255.append(float(row[5]))
+
 with open('results_l2_6', 'rb') as csvfile:
     plots = csv.reader(csvfile, delimiter = ' ')
     for row in plots:
@@ -73,6 +91,14 @@ with open('results_l2_6', 'rb') as csvfile:
         Ml26.append(float(row[3]))
         pl26.append(float(row[4]))
         el26.append(float(row[5]))
+
+with open('results_l2_65', 'rb') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ' ')
+    for row in plots:
+        Rl265.append(float(row[1]))
+        Ml265.append(float(row[3]))
+        pl265.append(float(row[4]))
+        el265.append(float(row[5]))
 
 with open('results_l2_7', 'rb') as csvfile:
     plots = csv.reader(csvfile, delimiter = ' ')
@@ -98,7 +124,7 @@ with open('mr.out', 'rb') as csvfile:
         pd.append(float(row[2]))
         ed.append(float(row[3]))
 
-plt.figure(figsize=(10,5))
+plt.figure(figsize=(14,7))
 
 el3n = [a*P for a in el3]
 pl3n = [a*P for a in pl3]
@@ -109,8 +135,14 @@ pl2n = [a*P for a in pl2]
 el25n = [a*P for a in el25]
 pl25n = [a*P for a in pl25]
 
+el255n = [a*P for a in el255]
+pl255n = [a*P for a in pl255]
+
 el26n = [a*P for a in el26]
 pl26n = [a*P for a in pl26]
+
+el265n = [a*P for a in el265]
+pl265n = [a*P for a in pl265]
 
 el27n = [a*P for a in el27]
 pl27n = [a*P for a in pl27]
@@ -121,13 +153,17 @@ pl28n = [a*P for a in pl28]
 p1 = np.arange(0., 0.00025, 0.00000001)
 
 plt.subplot(2,2,(1,3))
+plt.xlim(11.5, 14)
+plt.ylim(1.1, 1.2)
 plt.plot(Rd, Md, label = 'MRR input')
 #plt.plot(Rl2, Ml2,label = 'MRR $\lambda = 0.01$')
 plt.plot(Rl25, Ml25,label = 'MRR $\lambda = 0.05$')
+plt.plot(Rl255, Ml255,label = 'MRR $\lambda = 0.055$')
 plt.plot(Rl26, Ml26,label = 'MRR $\lambda = 0.06$')
+plt.plot(Rl265, Ml265,label = 'MRR $\lambda = 0.065$')
 plt.plot(Rl27, Ml27,label = 'MRR $\lambda = 0.07$')
 plt.plot(Rl28, Ml28,label = 'MRR $\lambda = 0.08$')
-#plt.plot(Rl3, Ml3,label = 'MRR $\lambda = 0.001$')
+plt.plot(Rl3, Ml3,label = 'MRR $\lambda = 0.001$')
 plt.ylabel('M/M$_\odot$', fontsize=15)
 plt.xlabel('R/km', fontsize=15)
 plt.legend(loc=1,prop={'size':12})
@@ -136,12 +172,14 @@ plt.subplot(2,2,(2,4))
 plt.plot(p1*P, eos(p1)*P, label = 'EOS input')
 #plt.plot(pl2n, el2n, label ='EOS $\lambda = 0.01$')
 plt.plot(pl25n, el25n, label ='EOS $\lambda = 0.05$')
+plt.plot(pl255n, el255n, label ='EOS $\lambda = 0.055$')
 plt.plot(pl26n, el26n, label ='EOS $\lambda = 0.06$')
+plt.plot(pl265n, el265n, label ='EOS $\lambda = 0.065$')
 plt.plot(pl27n, el27n, label ='EOS $\lambda = 0.07$')
 plt.plot(pl28n, el28n, label ='EOS $\lambda = 0.08$')
-#plt.plot(pl3n, el3n, label ='EOS $\lambda = 0.001$')
-plt.ylabel('$\epsilon$(p)/GeVfm$^{-3}$', fontsize=15)
-plt.xlabel('p/GeVfm$^{-3}$', fontsize=15)
+plt.plot(pl3n, el3n, label ='EOS $\lambda = 0.001$')
+plt.ylabel('$\epsilon$(p)/MeVfm$^{-3}$', fontsize=15)
+plt.xlabel('p/MeVfm$^{-3}$', fontsize=15)
 plt.legend(loc=2,prop={'size':12})
 
 plt.tight_layout()
